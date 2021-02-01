f4add57842ac8406b1f0d86f954e40c0a2ba0cd7
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-02-02 00:25:36 +0300

    refactoring

--- src/HireMe/HireMe.js
+++ src/HireMe/HireMe.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from './HireMe.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
 import {Button} from "../common/components/button/Button";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 
 export function HireMe() {
@@ -9,7 +10,15 @@ export function HireMe() {
         <div className={s.hireMe}>
             <div className={`${styleContainer.container} ${s.hireMeContainer}`}>
                 <h3>I am considering opportunity for <span>remote</span> work</h3>
-                <Button title={"Hire me"}/>
+                <Link
+                    activeClass={""}
+                    to="contacts"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}
+                >Hire me</Link>
+                {/*<Button title={"Hire me"}/>*/}
             </div>
         </div>
     )
